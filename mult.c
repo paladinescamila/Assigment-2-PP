108666c1ab6d893d38ad30a6453fc611ed7fd11d
paladinescamila <camilapaladines27@gmail.com>
2021-03-20 14:45:57 -0500

    Matrix multiplication without parallelism

--- mult.c
+++ mult.c
@@ -1,36 +1,58 @@
-#include<stdio.h>    
-#include<stdlib.h>  
+#include <stdio.h>
+#define N 10
 
-int main(){  
-    int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
-    system("cls");  
-    printf("Rows: ");    
-    scanf("%d",&r);
-    printf("Columns: ");    
-    scanf("%d",&c);
-    printf("First matrix element: \n");    
-    
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++) scanf("%d",&a[i][j]);
-    }
-    
-    printf("Second matrix element: \n");
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++) scanf("%d",&b[i][j]);    
-    }
-    
-    printf("Multiply of the matrix: \n");
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            mul[i][j]=0;
-            for(k=0;k<c;k++) mul[i][j]+=a[i][k]*b[k][j];
-        }
-    }
+// Get matrix elements
+void getMatrix(int matrix[][N], int rows, int cols){
+    for (int i = 0; i < rows; i++){
+        printf("Row %d: ", i+1);
+        for (int j = 0; j < cols; j++)
+            scanf("%d", &matrix[i][j]);
+   }
+}
 
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++) printf("%d\t",mul[i][j]);
-        printf("\n");    
+// Matrix multiplication
+void multiply(int one[][N], int two[][N], int ans[][N], int rows1, int cols1, int rows2, int cols2) {
+    for (int i = 0; i < rows1; i++){
+        for (int j = 0; j < cols2; j++)
+            ans[i][j] = 0;
     }
+    for (int i = 0; i < rows1; i++){
+        for (int j = 0; j < cols2; j++){
+            for (int k = 0; k < cols1; k++)
+                ans[i][j] += one[i][k] * two[k][j];
+      }
+   }
+}
 
+// Print the matrix
+void printMatrix(int ans[][N], int rows, int cols){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++)
+            printf("%d ", ans[i][j]);
+        printf("\n");
+    }
+}
+
+int main(){
+    int one[N][N], two[N][N], ans[N][N], rows1, cols1, rows2, cols2;
+    printf("Rows and columns matrix 1: ");
+    scanf("%d %d", &rows1, &cols1);
+    printf("Rows and columns matrix 2: ");
+    scanf("%d %d", &rows2, &cols2);
+    
+    if (cols1 == rows2){
+        printf("\nMATRIX 1\n");
+        getMatrix(one, rows1, cols1);
+        printf("\nMATRIX 2\n");
+        getMatrix(two, rows2, cols2);
+        
+        multiply(one, two, ans, rows1, cols1, rows2, cols2);
+
+        printf("\nRESULT\n");
+        printMatrix(ans, rows1, cols2);
+    }
+    else{
+        printf("The number of columns in matrix 1 must be the same as the number of rows in matrix 2.\n");
+    }
     return 0;
-} 
+}
