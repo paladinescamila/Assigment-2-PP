97663613db2f0106b9abfd316ced82bda628c516
paladinescamila <camilapaladines27@gmail.com>
2021-03-21 17:06:49 -0500

    Change some variable names

--- mult.c
+++ mult.c
@@ -13,45 +13,45 @@ void buildMatrix(int matrix[][N], int rows, int cols){
 }
 
 // Print the matrix
-void printMatrix(int ans[][N], int rows, int cols){
+void printMatrix(int matrix[][N], int rows, int cols){
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++)
-            printf("%d\t", ans[i][j]);
+            printf("%d\t", matrix[i][j]);
         printf("\n");
     }
 }
 
 int main(int argc, char *argv[]){
     // int num_threads = omp_get_max_threads(), tid;
-    int one[N][N], two[N][N], ans[N][N], rows1, cols1, rows2, cols2;
+    int A[N][N], B[N][N], C[N][N], r1, c1, r2, c2;
 
     printf("Rows and columns matrix 1: ");
-    scanf("%d %d", &rows1, &cols1);
+    scanf("%d %d", &r1, &c1);
     printf("Rows and columns matrix 2: ");
-    scanf("%d %d", &rows2, &cols2);
+    scanf("%d %d", &r2, &c2);
     
-    if (cols1 == rows2){
+    if (c1 == r2){
         printf("\nMATRIX 1\n");
-        buildMatrix(one, rows1, cols1);
-        printMatrix(one, rows1, cols1);
+        buildMatrix(A, r1, c1);
+        printMatrix(A, r1, c1);
         printf("\nMATRIX 2\n");
-        buildMatrix(two, rows2, cols2);
-        printMatrix(two, rows2, cols2);
+        buildMatrix(B, r2, c2);
+        printMatrix(B, r2, c2);
         
-        for (int i = 0; i < rows1; i++){
-            for (int j = 0; j < cols2; j++)
-                ans[i][j] = 0;
+        for (int i = 0; i < r1; i++){
+            for (int j = 0; j < c2; j++)
+                C[i][j] = 0;
         }
         #pragma omp parallel for
-        for (int i = 0; i < rows1; i++){
-            for (int j = 0; j < cols2; j++){
-                for (int k = 0; k < cols1; k++)
-                    ans[i][j] += one[i][k] * two[k][j];
+        for (int i = 0; i < r1; i++){
+            for (int j = 0; j < c2; j++){
+                for (int k = 0; k < c1; k++)
+                    C[i][j] += A[i][k] * B[k][j];
             }
         }
 
         printf("\nRESULT\n");
-        printMatrix(ans, rows1, cols2);
+        printMatrix(C, r1, c2);
     }
     else{
         printf("The number of columns in matrix 1 must be the same as the number of rows in matrix 2.\n");
