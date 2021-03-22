e2b0d390b7ae0e515aa370298fd5fb96223cb0df
paladinescamila <camilapaladines27@gmail.com>
2021-03-22 14:16:07 -0500

    Change number type

--- mult.c
+++ mult.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 500
-#define M 100
+#define M 10
+#define lli long long int
 
 
 // Build an array of random numbers with the given dimensions
-void buildMatrix(int matrix[N][N], int rows, int cols){
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++)
+void buildMatrix(int matrix[N][N], lli rows, lli cols){
+    for (lli i = 0; i < rows; i++){
+        for (lli j = 0; j < cols; j++)
             matrix[i][j] = rand() % M;
    }
 }
 
 
 // Print a matrix
-void printMatrix(int matrix[N][N], int rows, int cols){
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++)
+void printMatrix(int matrix[N][N], lli rows, lli cols){
+    for (lli i = 0; i < rows; i++){
+        for (lli j = 0; j < cols; j++)
             printf("%d\t", matrix[i][j]);
         printf("\n");
     }
@@ -25,17 +26,17 @@ void printMatrix(int matrix[N][N], int rows, int cols){
 
 
 // Multiply two matrices
-double multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], int r1, int c1, int r2, int c2){
+double multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], lli r1, lli c1, lli r2, lli c2){
     double t_start, t_wall_clock;
     // omp_set_nested(1);
     t_start = omp_get_wtime();
 
-    // #pragma omp parallel for
-    for (int i = 0; i < r1; i++){
+    #pragma omp parallel for
+    for (lli i = 0; i < r1; i++){
         // #pragma omp parallel for
-        for (int j = 0; j < c2; j++){
+        for (lli j = 0; j < c2; j++){
             C[i][j] = 0;
-            for (int k = 0; k < c1; k++)
+            for (lli k = 0; k < c1; k++)
                 C[i][j] += A[i][k] * B[k][j];
         }
     }
@@ -47,14 +48,15 @@ double multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], int r1, int c1, i
 
 // Tests with different dimensions of C
 int main(int argc, char *argv[]){
-    int A[N][N], B[N][N], C[N][N], max, iter;
-    scanf("%d %d", &max, &iter);
+    int A[N][N], B[N][N], C[N][N];
+    lli max, iter;
+    scanf("%lld %lld", &max, &iter);
 
-    for (int i = iter; i <= max; i += iter){
+    for (lli i = iter; i <= max; i += iter){
         buildMatrix(A, i, i);
         buildMatrix(B, i, i);
         double t_wall_clock = multiplyMatrices(A, B, C, i, i, i, i);
-        printf("Time (N = %d): %f\n", i, t_wall_clock);
+        printf("Time (N = %lld): %f\n", i, t_wall_clock);
     }
 
     
