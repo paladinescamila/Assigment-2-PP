ec7b8ba56d3bef9de3a09153440778c4087afdaf
paladinescamila <camilapaladines27@gmail.com>
2021-03-22 18:37:33 -0500

    Add data results

--- mult.c
+++ mult.c
@@ -5,7 +5,6 @@
 #define M 10
 #define lli long long int
 
-
 // Build an array of random numbers with the given dimensions
 void buildMatrix(int matrix[N][N], lli rows, lli cols){
     for (lli i = 0; i < rows; i++){
@@ -14,37 +13,23 @@ void buildMatrix(int matrix[N][N], lli rows, lli cols){
    }
 }
 
-
-// Print a matrix
-void printMatrix(int matrix[N][N], lli rows, lli cols){
-    for (lli i = 0; i < rows; i++){
-        for (lli j = 0; j < cols; j++)
-            printf("%d\t", matrix[i][j]);
-        printf("\n");
-    }
-}
-
-
 // Multiply two matrices
 double multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], lli r1, lli c1, lli r2, lli c2){
     double t_start, t_wall_clock;
-    // omp_set_nested(1);
+    omp_set_nested(1);
     t_start = omp_get_wtime();
-
-    // #pragma omp parallel for
+    #pragma omp parallel for
     for (lli i = 0; i < r1; i++){
-        // #pragma omp parallel for
+        #pragma omp parallel for
         for (lli j = 0; j < c2; j++){
             C[i][j] = 0;
             for (lli k = 0; k < c1; k++)
                 C[i][j] += A[i][k] * B[k][j];
         }
     }
-
     t_wall_clock = omp_get_wtime() - t_start;
     return t_wall_clock;
 }
-
 
 // Tests with different dimensions of C
 int main(int argc, char *argv[]){
@@ -53,8 +38,6 @@ int main(int argc, char *argv[]){
     buildMatrix(A, max, max);
     buildMatrix(B, max, max);
     double t_wall_clock = multiplyMatrices(A, B, C, max, max, max, max);
-    // printf("Time (N = %lld): %f\n", max, t_wall_clock);
-
-    
+    printf("Time (N = %lld): %f\n", max, t_wall_clock);
     return 0;
 }
