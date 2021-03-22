a8ca6024afb9511fdc4c07305b1aaf79ef9f9c2d
paladinescamila <camilapaladines27@gmail.com>
2021-03-21 19:59:49 -0500

    I don't know what I did

--- mult.c
+++ mult.c
@@ -4,18 +4,16 @@
 #define N 10
 #define M 100
 
-
 // Build an array of random numbers with the given dimensions
-void buildMatrix(int matrix[][N], int rows, int cols){
+void buildMatrix(int matrix[N][N], int rows, int cols){
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++)
             matrix[i][j] = rand() % M;
    }
 }
 
-
 // Print the matrix
-void printMatrix(int matrix[][N], int rows, int cols){
+void printMatrix(int matrix[N][N], int rows, int cols){
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++)
             printf("%d\t", matrix[i][j]);
@@ -23,9 +21,8 @@ void printMatrix(int matrix[][N], int rows, int cols){
     }
 }
 
-
 // Multiply two matrices
-void multiplyMatrices(int A[][N], int B[][N], int C[N][N], int r1, int c1, int r2, int c2){
+void multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], int r1, int c1, int r2, int c2){
     double t_start, t_wall_clock;
     for (int i = 0; i < r1; i++){
         for (int j = 0; j < c2; j++)
@@ -39,11 +36,11 @@ void multiplyMatrices(int A[][N], int B[][N], int C[N][N], int r1, int c1, int r
                 C[i][j] += A[i][k] * B[k][j];
         }
     }
-    t_wall_clock = omp_get_wtime() - t_startrt;
-    printf("%f", t_wall_clock);
+    t_wall_clock = omp_get_wtime() - t_start;
+    printf("Time: %f\n", t_wall_clock);
 }
 
-
+// Tests with different dimensions of C
 int main(int argc, char *argv[]){
     // int num_threads = omp_get_max_threads(), tid;
     int A[N][N], B[N][N], C[N][N], r1, c1, r2, c2;
