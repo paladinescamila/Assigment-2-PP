4a30b836711c836b4c379546278b279dc42087ce
paladinescamila <camilapaladines27@gmail.com>
2021-03-21 21:19:41 -0500

    Change the input structure

--- mult.c
+++ mult.c
@@ -1,7 +1,7 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define N 10
+#define N 500
 #define M 100
 
 // Build an array of random numbers with the given dimensions
@@ -22,7 +22,7 @@ void printMatrix(int matrix[N][N], int rows, int cols){
 }
 
 // Multiply two matrices
-void multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], int r1, int c1, int r2, int c2){
+double multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], int r1, int c1, int r2, int c2){
     double t_start, t_wall_clock;
     for (int i = 0; i < r1; i++){
         for (int j = 0; j < c2; j++)
@@ -37,34 +37,24 @@ void multiplyMatrices(int A[N][N], int B[N][N], int C[N][N], int r1, int c1, int
         }
     }
     t_wall_clock = omp_get_wtime() - t_start;
-    printf("Time: %f\n", t_wall_clock);
+    return t_wall_clock;
 }
 
 // Tests with different dimensions of C
 int main(int argc, char *argv[]){
-    // int num_threads = omp_get_max_threads(), tid;
-    int A[N][N], B[N][N], C[N][N], r1, c1, r2, c2;
+    int num_threads = omp_get_num_threads(), tid;
+    int A[N][N], B[N][N], C[N][N], max;
+    printf("Max size: ");
+    scanf("%d", &max);
 
-    printf("Rows and columns matrix A: ");
-    scanf("%d %d", &r1, &c1);
-    printf("Rows and columns matrix B: ");
-    scanf("%d %d", &r2, &c2);
-    
-    if (c1 == r2){
-        printf("\nMATRIX A:\n");
+    for (int i=100; i<=max; i+=100){
+        int  r1 = i, c1 = i, r2 = i, c2 = i;
         buildMatrix(A, r1, c1);
-        printMatrix(A, r1, c1);
-
-        printf("\nMATRIX B:\n");
         buildMatrix(B, r2, c2);
-        printMatrix(B, r2, c2);
+        double t_wall_clock = multiplyMatrices(A, B, C, r1, c1, r2, c2);
+        printf("Time (N = %d): %f\n", i, t_wall_clock);
+    }
 
-        printf("\nMATRIX C (A.B)\n");
-        multiplyMatrices(A, B, C, r1, c1, r2, c2);
-        printMatrix(C, r1, c2);
-    }
-    else{
-        printf("The number of columns in matrix 1 must be the same as the number of rows in matrix 2.\n");
-    }
+    
     return 0;
 }
